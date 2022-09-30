a442bb73de438ff49f8b50457ad5fed949c35bcc
Anatoly <gymbotan@rambler.ru>
2022-09-30 09:50:14 +0300

    Fix: Fix correct test data values.

--- TuviBytesShamirSecretSharingLib/TuviBytesShamirSecretSharingLibTests/TestCasesDataSource.cs
+++ TuviBytesShamirSecretSharingLib/TuviBytesShamirSecretSharingLibTests/TestCasesDataSource.cs
@@ -1,30 +1,20 @@
 ﻿using NUnit.Framework;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace TuviBytesShamirSecretSharingLibTests
 {
-    public static class TestCasesDataSource
+    internal static class TestCasesDataSource
     {
         public static IEnumerable<TestCaseData> TestCasesForBytesInterpolation
         {
             get
             {
-                yield return new TestCaseData( //y = x - 1
-                    8, 
-                    new (byte, byte)[] { (1, 0), (2, 3) }, 
-                    9);
-                yield return new TestCaseData( //y = x - 1
-                    13,
-                    new (byte, byte)[] { (1, 0), (2, 3) },
-                    12);
-                yield return new TestCaseData( //y = x^2 + 3x + 5
-                    3,
-                    new (byte, byte)[] { (0, 5), (1, 7), (2, 3) },
-                    15);
+                yield return new TestCaseData(
+                    (byte)8, new (byte, byte)[] { (1, 0), (2, 3) }, (byte)9); //y = x - 1
+                yield return new TestCaseData(
+                    (byte)13, new (byte, byte)[] { (1, 0), (2, 3) }, (byte)12); //y = x - 1
+                yield return new TestCaseData(
+                    (byte)3, new (byte, byte)[] { (0, 5), (1, 7), (2, 7) }, (byte)5); //y = x^2 + 3x + 5
             }
         }        
     }
