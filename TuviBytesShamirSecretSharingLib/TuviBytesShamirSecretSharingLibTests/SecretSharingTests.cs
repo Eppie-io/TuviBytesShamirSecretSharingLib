1982c5e06ebd9ed90ed99e99e70f94106a214fc3
gymbotan <gymbotan@rambler.ru>
2023-01-26 18:33:50 +0300

    Edit: Add link to external project. Change 2022 to 2023 in license text.

--- TuviBytesShamirSecretSharingLib/TuviBytesShamirSecretSharingLibTests/SecretSharingTests.cs
+++ TuviBytesShamirSecretSharingLib/TuviBytesShamirSecretSharingLibTests/SecretSharingTests.cs
@@ -1,5 +1,5 @@
 ///////////////////////////////////////////////////////////////////////////////
-//   Copyright 2022 Eppie (https://eppie.io)
+//   Copyright 2023 Eppie (https://eppie.io)
 //
 //   Licensed under the Apache License, Version 2.0(the "License");
 //   you may not use this file except in compliance with the License.
@@ -14,7 +14,6 @@
 //   limitations under the License.
 ///////////////////////////////////////////////////////////////////////////////
 
-using GF256Computations;
 using NUnit.Framework;
 using System;
 using TuviBytesShamirSecretSharingLib;
