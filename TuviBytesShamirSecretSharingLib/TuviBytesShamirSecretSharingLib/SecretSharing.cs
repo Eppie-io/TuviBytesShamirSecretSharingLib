668b3c6f09aefb0a219ed3a1e66b9995391c0c05
gymbotan <gymbotan@rambler.ru>
2023-04-28 16:00:45 +0300

    Correct CA1014.

--- TuviBytesShamirSecretSharingLib/TuviBytesShamirSecretSharingLib/SecretSharing.cs
+++ TuviBytesShamirSecretSharingLib/TuviBytesShamirSecretSharingLib/SecretSharing.cs
@@ -18,6 +18,7 @@ using GF256Computations;
 using System;
 using System.Security.Cryptography;
 
+[assembly: CLSCompliant(true)]
 namespace TuviBytesShamirSecretSharingLib
 {
     /// <summary>
--- TuviBytesShamirSecretSharingLib/TuviBytesShamirSecretSharingLibTests/SecretSharingTests.cs
+++ TuviBytesShamirSecretSharingLib/TuviBytesShamirSecretSharingLibTests/SecretSharingTests.cs
@@ -18,6 +18,7 @@ using NUnit.Framework;
 using System;
 using TuviBytesShamirSecretSharingLib;
 
+[assembly: CLSCompliant(true)]
 namespace TuviBytesShamirSecretSharingLibTests
 {
     public class SecretSharingTests
